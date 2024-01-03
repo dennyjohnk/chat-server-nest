ba41eacfb8840ca4cee67b0fe4479ea47d1b1809
Denny John <dennyjohn@zoho.com>
2024-01-03 18:18:04 +0530

    make login api public

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -10,12 +10,14 @@ import {
 } from '@nestjs/common';
 import { AuthGuard } from './auth.guard';
 import { AuthService } from './auth.service';
+import { Public } from './public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @HttpCode(HttpStatus.OK)
+  @Public()
   @Post('login')
   signIn(@Body() signInDto: Record<string, any>) {
     return this.authService.signIn(signInDto.username, signInDto.password);
