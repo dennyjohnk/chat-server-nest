84497a9af0272dd44049e4f2179c499750c4ff39
Denny John <dennyjohn@zoho.com>
2024-01-03 17:34:00 +0530

    swagger

--- src/chatbot/chatbot.controller.ts
+++ src/chatbot/chatbot.controller.ts
@@ -4,13 +4,17 @@ import {
   Param,
   HttpException,
   HttpStatus,
+  Logger,
 } from '@nestjs/common';
 
 @Controller('bot')
 export class ChatBotController {
+  private readonly logger = new Logger(ChatBotController.name);
+
   @Get(':id')
   async getBotInfo(@Param() params: any): Promise<string> {
     try {
+      this.logger.log('get bot called');
       return `${params.id}`;
     } catch (error) {
       throw new HttpException(
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
+import * as compression from 'compression';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Pelihealth')
+    .setDescription(`Pelihealth API's`)
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.use(compression());
   await app.listen(3000);
 }
 bootstrap();
