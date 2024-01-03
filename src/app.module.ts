2c39670596c0b53a8e963b693e1168b8c8764dd6
Denny John <dennyjohn@zoho.com>
2024-01-03 16:01:19 +0530

    logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,26 @@
-import { Module } from '@nestjs/common';
+import {
+  Module,
+  NestModule,
+  MiddlewareConsumer,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 
 import { AppService } from './app.service';
 import { CatsModule } from './cats.module';
 import { ChatbotModule } from './chatbot.module';
 
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
+
 @Module({
   imports: [CatsModule, ChatbotModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({ path: 'ab*cd', method: RequestMethod.ALL });
+  }
+}
